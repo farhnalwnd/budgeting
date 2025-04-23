ad71d855b4b3419b1a4462e6933b84a816ff57ec
aderusmana <aderusmana777@gmail.com>
2025-04-23 08:20:23 +0700

    Add Bavix Laravel Wallet integration and update migrations for transactions, transfers, and wallets

--- app/Models/Department.php
+++ app/Models/Department.php
@@ -2,13 +2,15 @@
 
 namespace App\Models;
 
+use Bavix\Wallet\Traits\HasWallet;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Str;
 
 class Department extends Model
 {
-    use HasFactory;
+
+    use HasFactory, HasWallet;
     protected $connection = 'mysql';
     protected $guarded = ['id'];
 
