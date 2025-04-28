2f70ec0e33e2b0467e388fcd780c984c974660a8
Farhan <raja.farhan.alwanda@gmail.com>
2025-04-28 10:40:08 +0700

    update penyesuaian

---
+++ app/Models/Budgeting/Purchase.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models\Budgeting;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Purchase extends Model
+{
+    use HasFactory;
+}
