0e38544573c9a472a454b7d6603f57c3232ea960
Farhan <raja.farhan.alwanda@gmail.com>
2025-04-29 17:01:29 +0700

    commit modal 4

--- resources/views/page/budgeting/management/PurchaseRequest/index.blade.php
+++ resources/views/page/budgeting/management/PurchaseRequest/index.blade.php
@@ -44,24 +44,24 @@
                     <table id="usersTable" class="table table-striped w-full text-left rtl:text-right table-bordered">
                         <thead class="uppercase border-b">
                             <tr>
-                                <th class="px-6 py-3 text-lg w-5">#</th>
-                                <th class="px-6 py-3 text-lg whitespace-nowrap w-10">NO Budget</th>
-                                <th class="px-6 py-3 text-lg">Item Name</th>
-                                <th class="px-6 py-3 text-lg w-5">Amount</th>
-                                <th class="px-6 py-3 text-lg w-5">Quantity</th>
-                                <th class="px-6 py-3 text-lg w-5">status</th>
-                                <th class="px-6 py-3 text-lg ">remarks</th>
+                                <th class="px-6 py-3 text-lg text-center w-5">#</th>
+                                <th class="px-6 py-3 text-lg text-center whitespace-nowrap w-10">NO Budget</th>
+                                <th class="px-6 py-3 text-lg text-center">Item Name</th>
+                                <th class="px-6 py-3 text-lg text-center w-48">Amount</th>
+                                <th class="px-6 py-3 text-lg text-center w-5">Quantity</th>
+                                <th class="px-6 py-3 text-lg text-center w-5">status</th>
+                                <th class="px-6 py-3 text-lg text-center ">remarks</th>
                             </tr>
                         </thead>
                         <tbody>
                             @foreach ($purchases as $purchase)
                             <tr>
-                                <td class="px-6 py-4 text-lg w-5">{{ $purchase->id }}</td>
-                                <td class="px-6 py-4 text-lg whitespace-nowrap w-10">{{ $purchase->budget_no }}</td>
+                                <td class="px-6 py-4 text-lg text-center w-5">{{ $purchase->id }}</td>
+                                <td class="px-6 py-4 text-lg text-center whitespace-nowrap w-10">{{ $purchase->budget_no }}</td>
                                 <td class="px-6 py-4 text-lg">{{ $purchase->item_name }}</td>
-                                <td class="px-6 py-4 text-lg w-5">{{ $purchase->amount }}</td>
-                                <td class="px-6 py-4 text-lg">{{ $purchase->quanitity }}</td>
-                                <td class="px-6 py-4 text-lg w-5">{{ $purchase->status }}</td>
+                                <td class="px-6 py-4 text-lg text-center w-48">Rp. {{ number_format($purchase->amount, 0, ',', '.') }}</td>
+                                <td class="px-6 py-4 text-lg text-center">{{ $purchase->quanitity }}</td>
+                                <td class="px-6 py-4 text-lg text-center w-5">{{ $purchase->status }}</td>
                                 <td class="px-6 py-4 text-lg w-2/6">{{ $purchase->remarks }}</td>
                                 </tr>
                             @endforeach
